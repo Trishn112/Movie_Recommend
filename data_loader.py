482de46adb0bfcfda9b92c42bab4ec8eeac0f131
Trishn <trishn@example.com>
2026-07-12 16:43:39 +0530

    Changes

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,21 @@
+import os
 import pickle
+import gdown
 import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
 
+SIM_FILE = "sim.pkl"
 
-@st.cache_data
+URL = f"https://drive.google.com/uc?id={FILE_ID}"
+
+@st.cache_resource
 def load_data():
+    if not os.path.exists(SIM_FILE):
+        print("Downloading similarity matrix...")
+        gdown.download(URL, SIM_FILE, quiet=False)
 
     movies = pickle.load(open("movie.pkl", "rb"))
-    similarity = pickle.load(open("sim.pkl", "rb"))
+    similarity = pickle.load(open(SIM_FILE, "rb"))
 
     return movies, similarity
